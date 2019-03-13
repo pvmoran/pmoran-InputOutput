d071aabd993d5d12a5757bf9371bd71bddcf3508
pvmoran <47060960+pvmoran@users.noreply.github.com>
2019-03-13 10:36:17 -0500

    Add files via upload

---
+++ script/myScript.js
@@ -0,0 +1,5 @@
+var name;
+
+name = prompt("Welcome, " + name);
+alert("Hello " + name + "! Welcome to the Web Dev 06 - Input & Output Assignment.");
+console.log("Hello " + name + "! Welcome to the Web Dev 06 - Input & Output Assignment.");
